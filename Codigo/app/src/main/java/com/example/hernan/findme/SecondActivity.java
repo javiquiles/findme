ec8cc5c4e5f2842ab145986a819d526ad70483e8
Javi <javiquiles@hotmail.com>
2016-06-16 16:52:22 -0300

    codigo_gps

--- Codigo/app/src/main/java/com/example/hernan/findme/SecondActivity.java
+++ Codigo/app/src/main/java/com/example/hernan/findme/SecondActivity.java
@@ -1,164 +1,104 @@
 package com.example.hernan.findme;
 
-import android.Manifest;
 import android.app.Activity;
+
 import android.content.Context;
-import android.media.AudioManager;
-import android.media.ToneGenerator;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
-import android.os.Handler;
-import android.os.Vibrator;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import android.location.Location;
+
+import android.location.LocationListener;
+
+import android.location.LocationManager;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.util.List;
+import android.widget.Toast;
 
-public class SecondActivity extends Activity {
 
-    protected TextView textView;
-    private String tesoro;
-    protected ImageView intensidad1;
-    protected ImageView intensidad2;
-    protected ImageView intensidad3;
-    final Handler handler = new Handler();
-    private final int MAX_TIME = 1000;
-    private final int MAX_DB = -90;
-    private final int MIN_DB = -20;
+public class SecondActivity extends Activity
+
+{
+
+    /** Called when the activity is first created. */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState)
+
+    {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second);
 
-        tesoro = getIntent().getExtras().getString("tesoro");
 
-        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
-        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.VIBRATE }, 1);
+/* Use the LocationManager class to obtain GPS locations */
 
-        WifiManager wifiManager;
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        LocationManager mlocManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
-        wifiManager.startScan();
+        LocationListener mlocListener = new MyLocationListener();
 
-        try {
-            run();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
 
-    }
-
-    public void run() throws InterruptedException {
-
-        textView = (TextView) findViewById(R.id.texto);
-
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int intensidad = leerIntensidad();
-                    Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                    textView.setText(String.valueOf(intensidad));
-                    cambiarIntensidad(intensidad);
-
-                    if (Math.abs(intensidad) > 0) {
-
-                        v.vibrate(getMillis(intensidad));
-                    }
-                    //ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, getMillis(intensidad));
-                    //toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, getMillis(intensidad));
-
-
-                    handler.postDelayed(this,2000);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        handler.post(runnable);
 
     }
 
 
-    public int leerIntensidad() throws InterruptedException {
-        List<ScanResult> redes;
-        WifiManager wifiManager;
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        wifiManager.startScan();
+/* Class My Location Listener */
 
-        redes = wifiManager.getScanResults();
-        int level = 0;
-        for(ScanResult red : redes)
+    public class MyLocationListener implements LocationListener
+
+    {
+
+        @Override
+
+        public void onLocationChanged(Location loc)
+
         {
-            if (red.SSID.equals(tesoro))
-            {
-                //Mostrar en pantalla el red.level
-                level = red.level;
-                break;
-            }
+
+            loc.getLatitude();
+
+            loc.getLongitude();
+
+            String Text = "My current location is: " + "Latitud = " + loc.getLatitude() + "Longitud = " + loc.getLongitude();
+
+            Toast.makeText( getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
         }
 
-        return level;
 
-    }
+        @Override
 
-    public int getMillis(int intensity) {
-        double intensityPercent = getIntensityPercent(intensity);
-        int millis = (int) (MAX_TIME * intensityPercent);
-        if (millis > MAX_TIME) {
-            millis = MAX_TIME;
+        public void onProviderDisabled(String provider)
+
+        {
+
+            Toast.makeText( getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+
         }
-        return millis;
-    }
 
-    public double getIntensityPercent(int intensity) {
-        double diffAvg = 0;
-        double intensityAbs = Math.abs(intensity);
-        if (intensityAbs < (MAX_TIME)) {
-            double maxDBAbs = Math.abs(MAX_DB);
-            double minDBAbs = Math.abs(MIN_DB);
-            double diff = maxDBAbs + minDBAbs - intensityAbs;
-            diffAvg = (diff / maxDBAbs);
+
+        @Override
+
+        public void onProviderEnabled(String provider)
+
+        {
+
+            Toast.makeText( getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+
         }
-        return diffAvg;
-    }
 
-    public void cambiarIntensidad(int intensity){
-        intensidad1 = (ImageView) findViewById(R.id.intensidad1);
-        intensidad2 = (ImageView) findViewById(R.id.intensidad2);
-        intensidad3 = (ImageView) findViewById(R.id.intensidad3);
-        TextView porcentaje = (TextView) findViewById(R.id.porcentaje);
 
-        double i = getIntensityPercent(intensity);
-        i *= 100;
+        @Override
 
-        porcentaje.setText(String.valueOf((int) i));
+        public void onStatusChanged(String provider, int status, Bundle extras)
 
-        if(i <= 25){
-            intensidad1.setVisibility(View.INVISIBLE);
-            intensidad2.setVisibility(View.INVISIBLE);
-            intensidad3.setVisibility(View.INVISIBLE);
-        }else if(i > 75){
-            intensidad1.setVisibility(View.VISIBLE);
-            intensidad2.setVisibility(View.VISIBLE);
-            intensidad3.setVisibility(View.VISIBLE);
-        }else if(i > 50){
-            intensidad1.setVisibility(View.VISIBLE);
-            intensidad2.setVisibility(View.VISIBLE);
-            intensidad3.setVisibility(View.INVISIBLE);
-        }else if(i > 25){
-            intensidad1.setVisibility(View.VISIBLE);
-            intensidad2.setVisibility(View.INVISIBLE);
-            intensidad3.setVisibility(View.INVISIBLE);
+        {
+
+
         }
-    }
+
+    }/* End of Class MyLocationListener */
+
+}/* End of UseGps Activity */
 }
 
